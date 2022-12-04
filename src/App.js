ee4bd3895f40eb47a34e1bf08d2b5d98e73d2131
Arishem <pkvaygon@gmail.com>
2022-12-04 17:46:25 +0600

    4dec.v1create@Ar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AboutUs from './pages/AboutUs/AboutUs';
+import Home from './pages/Home/Home';
+import Activities from './pages/Activities/Activities';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Contacts from './pages/Contacts/Contacts';
 
 const App = () => {
   return (
     <div>
-      asflapszf
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path='/activities' element={<Activities />} />
+        <Route path='/contacts' element={<Contacts />} />
+      </Routes>
+      <Footer />
     </div>
   );
 };
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from '../../Components/Main/Main';
+import Schedule from '../../Components/Schedule-section/Schedule';
+
+const Home = () => {
+    return (
+        <div>
+            <Main />
+            <Schedule />
+        </div>
+    );
+};
+
+export default Home;
