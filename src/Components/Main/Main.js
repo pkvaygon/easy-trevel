d4e0d88cad95b1a5d497327b4d752844062cee6b
Alukesh <akenzhebaev422@gmail.com>
2022-12-04 19:10:05 +0600

    4dec v1main@Al

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Main = () => {
     return (
-        <div>
+        <div className='main'>
             <h1 className='main__title'>Easy Travel Tur</h1>
         </div>
     );
